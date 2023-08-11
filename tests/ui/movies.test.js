aa9167d7894a759f7522c22e7b139980c62e6bad
Chavdar Zakev <chavdar.zakev@uniberg.com>
2023-08-11 17:54:32 +0300

    chnaged ui tests

--- tests/ui/movies.test.js
+++ tests/ui/movies.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check movies page', async ({ page }) => {
-  await page.goto('http://localhost:8080/movies'); // Adjust the URL as needed
+  await page.goto('https://johnnypiper-deployment.onrender.com:8080/movies'); // Adjust the URL as needed
   const list = await page.$('ul'); // Update the selector
   expect(list).toBeTruthy();
 });
