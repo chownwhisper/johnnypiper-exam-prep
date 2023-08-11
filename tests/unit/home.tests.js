91d44afcadcf97eea715cb1be4e4ad06243d019d
chownwhisper <120183774+chownwhisper@users.noreply.github.com>
2023-08-11 15:55:50 +0300

    Update home.tests.js

--- tests/unit/home.tests.js
+++ tests/unit/home.tests.js
@@ -5,12 +5,18 @@ suite('Home page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8888/");
     let body = await res.text();
-    assert.ok(body.includes("<h1>Welcome</h1>"));
+    
+    // Debugging: Log the fetched HTML content
+    console.log("Fetched HTML:", body);
+    
+    assert.ok(body.includes("<h1>Welcome</h1>"), "Page title not found");
   });
   
   test('Movies count', async function() {
     let res = await fetch("http://localhost:8888/");
     let body = await res.text();
-    assert.ok(body.includes("Added movies: <b>3</b>"));
+    
+    assert.ok(body.includes("Added movies: <b>3</b>"), "Movies count not found");
   });
 });
+
